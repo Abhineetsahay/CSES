f44a45c53486e05db3ef66045a139865afa1462b
Abhineetsahay <abhineetsahay@gmail.com>
2025-07-23 23:42:35 +0530

    Subarray Sums 1 and 2

---
+++ Sorting and Searching/SubarraySums1and2.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define fastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define all(s) s.begin(), s.end()
+
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    map<ll, ll> mp;
+    mp[0] = 1;
+    ll ans = 0, sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += a[i];
+        ll fnd = sum - k;
+
+        if (mp.find(fnd) != mp.end())
+        {
+            ans += mp[fnd];
+        }
+        mp[sum]++;
+    }
+    cout << ans << '\n';
+}
+
+int main()
+{
+    fastIO;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
